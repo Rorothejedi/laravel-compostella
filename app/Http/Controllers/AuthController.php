c5a75864dac3fa50b935264cf9ae100f77083c2d
r.cabotiau@intech-so.fr <rodolphe.cabotiau@gmail.com>
2022-03-16 15:16:14 +0100

    Add recaptcha on login auth method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,6 +14,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $fields = $request->validate([
+            'g-recaptcha-response' => 'required|captcha',
             'email' => 'required|string',
             'password' => 'required|string'
         ]);
